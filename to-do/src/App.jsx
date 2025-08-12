c2296891a57b4fc750fe234b21f2a05905774b6e
Gevorg Arzakanyan <zokavski@gmail.com>
2025-08-12 22:17:24 +0400

    task done, except little bags

--- to-do/src/App.jsx
+++ to-do/src/App.jsx
@@ -1,35 +1,67 @@
 import { useState } from "react";
 import "./App.css";
+import { FileSource } from "@google/genai";
 
 function App() {
-  const [tasks, setTasks] = useState([
-    { title: "start the task", complete: true },
-    { title: "finish the task", complete: false },
-  ]);
+  const [tasks, setTasks] = useState([]);
 
-  const [formData, setFormData] = useState({ title: "", complete: false });
+  const [formData, setFormData] = useState({
+    title: "",
+    complete: false,
+    id: undefined,
+  });
 
   function saveInput(e) {
     setFormData((prev) => ({ ...prev, title: e.target.value }));
-    console.log(formData.title);
   }
 
   function saveStatus() {
     setFormData((prev) => ({ ...prev, complete: !prev.complete }));
-    console.log(formData.complete);
+  }
+
+  function deleteTask() {
+    setShowInput("");
+    if (formData.title === "") return;
+    setTasks(tasks.filter((task, index) => index !== formData.id));
   }
 
   function saveTask() {
+    setShowInput("");
     if (formData.title === "") return;
-    setTasks((prev) => [...prev, formData]);
-    setFormData({ title: "", complete: false });
+    if (formData.id === undefined) {
+      formData.id = tasks.length;
+      setTasks((prev) => [...prev, formData]);
+      setFormData({ title: "", complete: false, id: undefined });
+      return;
+    }
+    setTasks(
+      tasks.map((task, index) => (index === formData.id ? formData : task))
+    );
+    setFormData({ title: "", complete: false, id: undefined });
   }
+
+  function editTask(id) {
+    setShowInput("show");
+    setShowBtn("show-block");
+    setFormData(tasks[id]);
+  }
+
+  const [showInput, setShowInput] = useState("");
+  const [showBtn, setShowBtn] = useState("");
 
   return (
     <>
-      <div className={`sider ${close ? "open-slider" : ""}`}>
-        <button className="add-task">ADD NEW TASK</button>
-        <form className="form">
+      <div className="sider">
+        <button
+          onClick={() => {
+            setShowInput("show");
+            setShowBtn("");
+          }}
+          className="add-task"
+        >
+          ADD NEW TASK
+        </button>
+        <form className={`form ${showInput}`}>
           <input
             onChange={saveInput}
             className="task-title"
@@ -43,21 +75,32 @@ function App() {
               onClick={() => {
                 saveTask();
               }}
-              className="option-btn"
+              className="option-btn show-block"
               type="button"
             >
               SAVE
             </button>
-            <button className="option-btn" type="button">
+            <button
+              onClick={deleteTask}
+              className={`option-btn ${showBtn}`}
+              type="button"
+            >
               DELETE
             </button>
-            <button onClick={saveStatus} className="option-btn" type="button">
+            <button
+              onClick={saveStatus}
+              className={`option-btn ${showBtn}`}
+              type="button"
+            >
               DONE
             </button>
           </div>
         </form>
         {tasks.map((task, index) => (
           <button
+            onClick={() => {
+              editTask(task.id);
+            }}
             key={index}
             className={`task ${task.complete ? "task-complete" : ""}`}
           >
